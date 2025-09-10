376f9b86a39e9dd398304aa16bfde8d5871da7f6
blueweb3-source <blueweb3@proton.me>
2025-09-09 22:44:27 -0700

    Create index.tsx

---
+++ golf-addicts-site-updated/pages/pages/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function HomeFallback() {
+  return (
+    <div style={{padding:24,fontFamily:'system-ui,sans-serif'}}>
+      <h1>Golf Addicts</h1>
+      <p>If you can read this, routing works.</p>
+    </div>
+  );
+}
